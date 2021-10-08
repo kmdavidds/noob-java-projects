40be011e5da6d16cd3ac605e5049dacf819ba712
kmdavidds <91604889+kmdavidds@users.noreply.github.com>
2021-10-08 12:11:26 +0800

    Update Main.java
    added comments

--- Main.java
+++ Main.java
@@ -15,29 +15,39 @@ public class Main {
      * @return String indicating the outcome of the game: "X wins" or "O wins" or "Tie" or "None"
      */
     public String checkGameWinner(char [][]grid) {
+        // None means the game continues
         String result = "None";
+        
         //Student code goes here ...
+        // setting the message of the end of the game
         String x = "X wins";
         String o = "O wins";
+        
+        // checks if there are three x vertically
         for (int i = 0; i < 3; i++) {
             if (grid[i][0] == 'x' && grid[i][1] == 'x' && grid[i][2] == 'x') {
                 result = x;
                 break;
             }
         }
+        // checks if there are three x horizontally
         for (int j = 0; j < 3; j++) {
             if (grid[0][j] == 'x' && grid[1][j] == 'x' && grid[2][j] == 'x') {
                 result = x;
                 break;
             }
         }
-
+        
+        // checks there are three x in the first diagonal
         if (grid[0][0] == 'x' && grid[1][1] == 'x' && grid[2][2] == 'x') {
             result = x;
         }
+        // checks there are three x in the second diagonal
         if (grid[2][0] == 'x' && grid[1][1] == 'x' && grid[0][2] == 'x') {
             result = x;
         }
+        
+        // this code up to line 68 does the same thing as the above, but with o
         for (int i2 = 0; i2 < 3; i2++) {
             if (grid[i2][0] == 'o' && grid[i2][1] == 'o' && grid[i2][2] == 'o') {
                 result = o;
@@ -56,6 +66,9 @@ public class Main {
         if (grid[2][0] == 'o' && grid[1][1] == 'o' && grid[0][2] == 'o') {
             result = o;
         }
+        
+        // checks for non-empty spaces ('-'), and if there are 9 non-empty spaces
+        // it means that the game is a tie
         int checkTieNum = 0;
         for (int i3 = 0; i3 < 3; i3++) {
             for (int j3 = 0; j3 < 3; j3++) {
@@ -67,7 +80,8 @@ public class Main {
         if (checkTieNum == 9) {
             result = "It's a tie";
         }
-
+        
+        // return the display message of the end of the game
         return result;
     }
 }
